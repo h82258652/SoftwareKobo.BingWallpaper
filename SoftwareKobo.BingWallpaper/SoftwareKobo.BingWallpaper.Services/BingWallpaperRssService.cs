fd898a0c83fdfea10f82640e594150cab781a700
h82258652 <842053625@qq.com>
2015-02-16 23:51:40 +0800

    添加Rss服务接口（未实现）

---
+++ SoftwareKobo.BingWallpaper/SoftwareKobo.BingWallpaper.Services/BingWallpaperRssService.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SoftwareKobo.BingWallpaper.Model;
+using SoftwareKobo.BingWallpaper.Services.Interfaces;
+
+namespace SoftwareKobo.BingWallpaper.Services
+{
+    public class BingWallpaperRssService : IBingWallpaperService
+    {
+        public Task<ImageArchiveCollection> GetWallpaperInformationsAsync(int daysAgo, int count, System.Globalization.CultureInfo area)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}

93862c27a5f956e69caecd7b8f0ea22f1d6b56a6
h82258652 <842053625@qq.com>
2015-03-05 17:24:31 +0800

    fix image load fail by mulitple network

--- SoftwareKobo.BingWallpaper/SoftwareKobo.BingWallpaper.WindowsPhone/Views/WallpaperHub.xaml.cs
+++ SoftwareKobo.BingWallpaper/SoftwareKobo.BingWallpaper.WindowsPhone/Views/WallpaperHub.xaml.cs
@@ -1,7 +1,10 @@
 ﻿// “用户控件”项模板在 http://go.microsoft.com/fwlink/?LinkId=234236 上提供
 
 using SoftwareKobo.BingWallpaper.ViewModels;
+using System;
+using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Media.Imaging;
 
 namespace SoftwareKobo.BingWallpaper.Views
 {
@@ -18,6 +21,19 @@ namespace SoftwareKobo.BingWallpaper.Views
         public WallpaperHub()
         {
             this.InitializeComponent();
+        }
+
+        private void Image_ImageFailed(object sender, ExceptionRoutedEventArgs e)
+        {
+            // 多并发下会导致图片加载失败。
+            Image image = (Image)sender;
+            BitmapImage bitmap = image.Source as BitmapImage;
+            if (bitmap != null)
+            {
+                string thumbUrl = bitmap.UriSource.OriginalString;
+                // 重新设定 Image 的源，加载缩略图。
+                image.Source = new BitmapImage(new Uri(thumbUrl, UriKind.Absolute));
+            }
         }
     }
 }

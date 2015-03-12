46aa4d5b20d332bf93e13834b1998ba269083dde
h82258652 <842053625@qq.com>
2015-03-13 02:13:41 +0800

    转移壁纸集合至全局

---
+++ SoftwareKobo.BingWallpaper/SoftwareKobo.BingWallpaper.WindowsPhone/Datas/Global.cs
@@ -0,0 +1,20 @@
+﻿using GalaSoft.MvvmLight.Ioc;
+using SoftwareKobo.BingWallpaper.Model;
+using SoftwareKobo.BingWallpaper.Services.Interfaces;
+
+namespace SoftwareKobo.BingWallpaper.Datas
+{
+    public class Global
+    {
+        private static WallpaperCollection _wallpaperCollection;
+
+        public static WallpaperCollection WallpaperCollection
+        {
+            get
+            {
+                _wallpaperCollection = _wallpaperCollection ?? new WallpaperCollection(SimpleIoc.Default.GetInstance<IBingWallpaperService>());
+                return _wallpaperCollection;
+            }
+        }
+    }
+}

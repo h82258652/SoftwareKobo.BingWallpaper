b9bbe14df00524cea9117d11f193855619b0d72c
h82258652 <842053625@qq.com>
2015-03-21 02:50:15 +0800

    为后台任务添加获取设置区域

---
+++ SoftwareKobo.BingWallpaper/SoftwareKobo.BingWallpaper.WindowsPhone.BackgroundTask/Settings.cs
@@ -0,0 +1,25 @@
+﻿using System.Globalization;
+using Windows.Foundation.Collections;
+using Windows.Storage;
+
+namespace SoftwareKobo.BingWallpaper.BackgroundTask
+{
+    public static class Settings
+    {
+        internal static string Area
+        {
+            get
+            {
+                IPropertySet roamingSettings = ApplicationData.Current.RoamingSettings.Values;
+                if (roamingSettings.ContainsKey("Area"))
+                {
+                    return (string)roamingSettings["Area"];
+                }
+                else
+                {
+                    return CultureInfo.CurrentCulture.Name;
+                }
+            }
+        }
+    }
+}

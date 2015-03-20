402894792b6d9f68525ab42c49d549208845ced5
h82258652 <842053625@qq.com>
2015-03-21 03:01:26 +0800

    添加两个转换器

---
+++ SoftwareKobo.BingWallpaper/SoftwareKobo.BingWallpaper.WindowsPhone/Converters/WallpaperCopyrightConverter.cs
@@ -0,0 +1,21 @@
+﻿using SoftwareKobo.BingWallpaper.Model;
+using SoftwareKobo.BingWallpaper.Services;
+using System;
+using Windows.UI.Xaml.Data;
+
+namespace SoftwareKobo.BingWallpaper.Converters
+{
+    public class WallpaperCopyrightConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, string language)
+        {
+            ImageArchive imageArchive = (ImageArchive)value;
+            return imageArchive.GetCopyright();
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, string language)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ SoftwareKobo.BingWallpaper/SoftwareKobo.BingWallpaper.WindowsPhone/Converters/WallpaperTitleConverter.cs
@@ -0,0 +1,21 @@
+﻿using SoftwareKobo.BingWallpaper.Model;
+using SoftwareKobo.BingWallpaper.Services;
+using System;
+using Windows.UI.Xaml.Data;
+
+namespace SoftwareKobo.BingWallpaper.Converters
+{
+    public class WallpaperTitleConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, string language)
+        {
+            ImageArchive imageArchive = (ImageArchive)value;
+            return imageArchive.GetTitle();
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, string language)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}

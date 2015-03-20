63bb242eb804232bca07fd53df05b66df6f67b0a
h82258652 <842053625@qq.com>
2015-03-21 03:02:27 +0800

    扩展壁纸信息扩展类

--- SoftwareKobo.BingWallpaper/SoftwareKobo.BingWallpaper.Services/ImageArchiveExtensions.cs
+++ SoftwareKobo.BingWallpaper/SoftwareKobo.BingWallpaper.Services/ImageArchiveExtensions.cs
@@ -1,23 +1,26 @@
 ﻿using SoftwareKobo.BingWallpaper.Model;
 using SoftwareKobo.BingWallpaper.Services.Helpers;
 using System;
-using System.Collections.Generic;
-using System.Globalization;
 using System.Linq;
-using System.Text;
 
 namespace SoftwareKobo.BingWallpaper.Services
 {
     public static class ImageArchiveExtensions
     {
-        public static DateTime GetStartDate(this ImageArchive imageArchive)
+        /// <summary>
+        /// 截取 copyright。
+        /// </summary>
+        /// <param name="imageArchive"></param>
+        /// <returns></returns>
+        public static string GetCopyright(this ImageArchive imageArchive)
         {
-            return DateTimeHelper.Parse(imageArchive.StartDate);
-        }
-
-        public static DateTime GetFullStartDate(this ImageArchive imageArchive)
-        {
-            return DateTimeHelper.Parse(imageArchive.FullStartDate);
+            string copyright = imageArchive.Copyright;
+            int index = copyright.IndexOf('(');
+            if (index > -1)
+            {
+                copyright = copyright.Substring(0, index).Trim();
+            }
+            return copyright;
         }
 
         public static DateTime GetEndDate(this ImageArchive imageArchive)
@@ -25,9 +28,47 @@ namespace SoftwareKobo.BingWallpaper.Services
             return DateTimeHelper.Parse(imageArchive.EndDate);
         }
 
+        /// <summary>
+        /// 如果存在 message，则设置为第一个 message 的 text。否则返回 null。
+        /// </summary>
+        /// <param name="imageArchive"></param>
+        /// <returns></returns>
+        public static string GetFirstMessageIfExist(this ImageArchive imageArchive)
+        {
+            Message[] messages = imageArchive.Messages;
+            return messages == null
+                ? null
+                : messages.Select(temp => temp.Text).FirstOrDefault(temp => string.IsNullOrEmpty(temp) == false);
+        }
+
+        public static DateTime GetFullStartDate(this ImageArchive imageArchive)
+        {
+            return DateTimeHelper.Parse(imageArchive.FullStartDate);
+        }
+
+        public static DateTime GetStartDate(this ImageArchive imageArchive)
+        {
+            return DateTimeHelper.Parse(imageArchive.StartDate);
+        }
+
+        /// <summary>
+        /// 如果存在 message，则以 message 作为壁纸的标题，否则截取 copyright。
+        /// </summary>
+        /// <param name="imageArchive"></param>
+        /// <returns></returns>
+        public static string GetTitle(this ImageArchive imageArchive)
+        {
+            string title = imageArchive.GetFirstMessageIfExist();
+            if (string.IsNullOrEmpty(title))
+            {
+                title = imageArchive.GetCopyright();
+            }
+            return title;
+        }
+
         public static string GetUrlWithSize(this ImageArchive imageArchive, WallpaperSize size)
         {
-            if (imageArchive==null)
+            if (imageArchive == null)
             {
                 return null;
             }

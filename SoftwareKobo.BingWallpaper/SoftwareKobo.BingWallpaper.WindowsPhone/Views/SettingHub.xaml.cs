23f74c12cb1ddd6a6e2fa7ce1ed6a41c1c3506f9
h82258652 <842053625@qq.com>
2015-03-21 16:31:02 +0800

    更改国家/地区后，刷新所有信息

--- SoftwareKobo.BingWallpaper/SoftwareKobo.BingWallpaper.WindowsPhone/Views/SettingHub.xaml.cs
+++ SoftwareKobo.BingWallpaper/SoftwareKobo.BingWallpaper.WindowsPhone/Views/SettingHub.xaml.cs
@@ -1,6 +1,7 @@
-﻿using Windows.UI.Xaml.Controls;
-
-// “用户控件”项模板在 http://go.microsoft.com/fwlink/?LinkId=234236 上提供
+﻿// “用户控件”项模板在 http://go.microsoft.com/fwlink/?LinkId=234236 上提供
+using SoftwareKobo.BingWallpaper.Datas;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
 
 namespace SoftwareKobo.BingWallpaper.Views
 {
@@ -9,6 +10,24 @@ namespace SoftwareKobo.BingWallpaper.Views
         public SettingHub()
         {
             this.InitializeComponent();
+        }
+
+        private void CmbArea_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            // 重新加载壁纸信息。
+            Global.WallpaperCollection.Reset();
+
+            Frame rootFrame = Window.Current.Content as Frame;
+            if (rootFrame != null)
+            {
+                MainPage mainPage = rootFrame.Content as MainPage;
+                if (mainPage != null)
+                {
+                    // 重新加载主页背景。
+                    // 更新主磁贴为所选区域。
+                    mainPage.ViewModel.Init();
+                }
+            }
         }
     }
 }
